1b21543adc7e1ce109ddefb651d53c3d2c53a5aa
Maitrey Prajapati <maitrey2112@gmail.com>
2019-01-14 00:13:10 +0530

    Basic html/css-no functionalities added

---
+++ calc.js
@@ -0,0 +1,14 @@
+const form = document.getElementById('form').addEventListener('submit', defineStuff);
+let op;
+function defineStuff(e){
+	e.preventDefault();
+	const d1 = document.getElementById('dig1').value;
+	const d2 = document.getElementById('dig2').value;
+	if(!d1){alert("d1 needed");}
+	if(!d2){alert("d2 needed");}
+	console.log(e);
+
+	
+
+
+}
